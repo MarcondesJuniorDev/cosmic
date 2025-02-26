457ec12a685e9f893f321fd60509c7b28c3966d1
MarcondesJuniorDev <mncjdev@gmail.com>
2025-02-26 09:18:37 -0400

    adiciona configuração de perfil ao painel administrativo

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->profile(isSimple: false)
             ->sidebarCollapsibleOnDesktop()
             ->colors([
                 'primary' => Color::Fuchsia,
