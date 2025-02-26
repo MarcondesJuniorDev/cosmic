759cd1a2cf8b6783363c96b6182804eeffac7c35
MarcondesJuniorDev <mncjdev@gmail.com>
2025-02-26 13:42:46 -0400

    adiciona método de redirecionamento na página de criação de usuário

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -10,4 +10,9 @@ use Illuminate\Support\Facades\Hash;
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
